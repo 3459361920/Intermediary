faccf9cb7a9aae43712e4406839a207ad89b1c98
3459361920 <3459361920@qq.com>
2020-06-16 21:05:17 +0800

    后台布局

---
+++ 毕业项目资料/项目源码和数据库/MVC卓越/intermediary/Controllers/BackstageController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace intermediary.Controllers
+{
+    public class BackstageController : Controller
+    {
+        // GET: Backstage
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Indexs()
+        {
+            return View();
+        }
+    }
+}
